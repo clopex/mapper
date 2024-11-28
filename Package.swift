7267693cd7047e5067972334953bfb967547b33c
Adis Mulabdic <adis.mulabdic@gmail.com>
2024-11-28 13:22:40 +0100

    Convert Mapper to Swift Package
    Convert Mapper to Swift Package

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Mapper",
+    platforms: [
+            .iOS(.v11), .macOS(.v10_14), .tvOS(.v11), .watchOS(.v4)
+        ],
     products: [
         .library(name: "Mapper", targets: ["Mapper"]),
     ],
